c942fa1e61fea02c0147c69e78e23507613e3790
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2020-09-22 22:24:04 -0300

    Show number on the display

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -62,27 +62,83 @@ class CalcController {
 
         }
 
+        calc(){
+
+            let last = this._operation.pop();
+
+            let result = eval(this._operation.join(''));
+
+            this._operation = [result, last];
+
+            this.setLastNumberToDisplay();
+        }
+
+        pushOperation(value){
+
+            this._operation.push(value);
+
+            if(this._operation.length > 3){
+             
+                 this.calc();
+
+            }else{
+
+
+            }
+        }
+
+        setLastNumberToDisplay(){
+
+            let lastNumber;
+
+            for(let i = this._operation.length -1; i >= 0; i--){
+
+                if(!this.isOperator(this._operation[i])){
+
+                    lastNumber = this._operation[i];
+                    break;
+
+                }
+            }
+
+            this.displayCalc = lastNumber;
+
+        }
+
         addOperator(value){
 
             if(isNaN(this.getLastOperation())){
 
-               if(this.isOperator()){
+               if(this.isOperator(value)){
 
                     this.setLastOperation(value);
 
                }else if(isNaN(value)){
                   
-                   
+                    console.log(value)
 
                }else{
 
-                    this._operation.push(value);
+                    this.pushOperation(value);
+
+                    this.setLastNumberToDisplay();
 
                }
 
             }else{
-                let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+
+                if(this.isOperator(value)){
+
+                    this.pushOperation(value);
+
+                }else{
+
+                    let newValue = this.getLastOperation().toString() + value.toString();
+                    this.setLastOperation(parseInt(newValue));
+
+                    this.setLastNumberToDisplay();
+
+                }
             }
              
             console.log(this._operation);
