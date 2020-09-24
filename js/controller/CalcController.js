73d5c882d971f0d0269f2d886d1e376d3fa27e9e
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2020-09-23 22:54:28 -0300

    Percent operation

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -21,6 +21,8 @@ class CalcController {
                 this.setDisplayDateTime();
  
             },1000);
+
+            this.setLastNumberToDisplay();
             
         }
 
@@ -36,10 +38,14 @@ class CalcController {
 
         clearAll(){
             this._operation = [];
+
+            this.setLastNumberToDisplay();
         }
 
         clearEntry(){
              this._operation.pop();
+
+             this.setLastNumberToDisplay();
         }
 
         setError(){
@@ -64,11 +70,29 @@ class CalcController {
 
         calc(){
 
-            let last = this._operation.pop();
+            let last = '';
 
+            if(this._operation.length > 3){
+
+                last = this._operation.pop();
+
+            }
+            
             let result = eval(this._operation.join(''));
 
-            this._operation = [result, last];
+            if(last == "%"){
+
+                result /= 100;
+
+                this._operation = [result];
+
+            }else{
+
+                this._operation = [result];
+
+                if(last) this._operation.push(last);
+                
+            }
 
             this.setLastNumberToDisplay();
         }
@@ -101,6 +125,8 @@ class CalcController {
                 }
             }
 
+            if(!lastNumber) lastNumber = 0;
+            
             this.displayCalc = lastNumber;
 
         }
@@ -169,7 +195,7 @@ class CalcController {
                     this.addOperator('%');
                         break;    
                 case 'igual':
-                    
+                    this.calc();
                     break;
                 case 'ponto':
                     this.addOperator('.');
