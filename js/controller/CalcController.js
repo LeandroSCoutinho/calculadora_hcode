c1320e3b3162c90dc3578404d03e3007e7d0dffa
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2020-09-30 21:28:55 -0300

    Adding audio button

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -1,6 +1,9 @@
 class CalcController {
     
     constructor(){
+
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator ='';
         this._lasNumber = '';
 
@@ -53,12 +56,40 @@ class CalcController {
             },1000);
 
             this.setLastNumberToDisplay();
-            this.pasteFromClipboard()
+            this.pasteFromClipboard();
+
+            document.querySelectorAll('btn-ac').forEach(btn =>{
+
+                btn.addEventListener('dblclick', e=>{
+
+                    this.toggleAudio();
+                });
+            });
             
+        }
+
+        toggleAudio(){
+
+            this._audioOnOff = !this._audioOnOff;
+            console.log(this._audioOnOff);
+
+        }
+
+        playAudio(){
+
+            if(this._audioOnOff){
+
+                this._audio.currentTime = 0;
+                this._audio.play();
+
+            }
+
         }
 
         initKeyBoard(){
             document.addEventListener('keyup', e =>{
+
+                this.playAudio();
 
                 switch(e.key){
                     case 'Escape':
@@ -295,6 +326,8 @@ class CalcController {
         }
 
         execBtn(value){
+
+            this.playAudio();
 
             switch(value){
                 case 'ac':
